4e6007fc0022739627e97182a13251997b8e7f10
SangWon7242 <gotkdqja@gmail.com>
2025-06-14 11:26:15 +0900

    게시물 등록시 id 값 증가

--- src/main/java/com/sbs/java/board/Main.java
+++ src/main/java/com/sbs/java/board/Main.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+    int articleLastId = 0;
 
     System.out.println("== 자바 CRUD 게시판 ==");
 
     while (true) {
       System.out.print("명령) ");
       String cmd = sc.nextLine();
-      System.out.printf("입력받은 명령어 : %s\n", cmd);
       
       if(cmd.equals("/usr/article/write")) {
         System.out.println("== 게시물 작성 ==");
@@ -21,12 +21,16 @@ public class Main {
         System.out.print("내용 : ");
         String content = sc.nextLine();
 
-        int id = 1;
+        int id = ++articleLastId;
+
         System.out.printf("%d번 게시물이 등록되었습니다.\n", id);
       }
       else if(cmd.equals("exit")) {
         System.out.println("프로그램을 종료합니다.");
         break;
+      }
+      else {
+        System.out.println("잘못 입력된 명령어입니다.");
       }
     }
 
