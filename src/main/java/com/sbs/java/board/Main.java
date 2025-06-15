3ffee50cfa130f79e83d653870cc21a151c23c55
SangWon7242 <gotkdqja@gmail.com>
2025-06-15 09:20:40 +0900

    usr/article/detail 기능 구현

--- src/main/java/com/sbs/java/board/Main.java
+++ src/main/java/com/sbs/java/board/Main.java
@@ -6,19 +6,20 @@ public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int articleLastId = 0;
+    Article lastArticle = null;
 
     System.out.println("== 자바 CRUD 게시판 ==");
 
     while (true) {
       System.out.print("명령) ");
       String cmd = sc.nextLine();
-      
-      if(cmd.equals("/usr/article/write")) {
+
+      if (cmd.equals("/usr/article/write")) {
         System.out.println("== 게시물 작성 ==");
         System.out.print("제목 : ");
         String subject = sc.nextLine();
 
-        if(subject.trim().isEmpty()) {
+        if (subject.trim().isEmpty()) {
           System.out.println("제목을 입력해주세요.");
           continue;
         }
@@ -26,7 +27,7 @@ public class Main {
         System.out.print("내용 : ");
         String content = sc.nextLine();
 
-        if(content.trim().isEmpty()) {
+        if (content.trim().isEmpty()) {
           System.out.println("내용을 입력해주세요.");
           continue;
         }
@@ -34,16 +35,32 @@ public class Main {
         int id = ++articleLastId;
 
         Article article = new Article(id, subject, content);
+        lastArticle = article;
 
         System.out.println("입력 된 게시물 객체 : " + article);
 
         System.out.printf("%d번 게시물이 등록되었습니다.\n", article.id);
-      }
-      else if(cmd.equals("exit")) {
+      } else if (cmd.equals("/usr/article/detail")) {
+        System.out.println("== 게시물 상세보기 ==");
+
+        // lastArticle은 마지막 게시물 객체의 주소값을 가진다.
+        Article article = lastArticle;
+
+        if(article == null) {
+          System.out.println("게시물이 존재하지 않습니다.");
+          continue;
+        }
+
+        System.out.printf("== %d번 게시물 조회 ==\n", article.id);
+        System.out.printf("번호 : %d\n", article.id);
+        System.out.printf("제목 : %s\n", article.subject);
+        System.out.printf("내용 : %s\n", article.content);
+
+
+      } else if (cmd.equals("exit")) {
         System.out.println("프로그램을 종료합니다.");
         break;
-      }
-      else {
+      } else {
         System.out.println("잘못 입력된 명령어입니다.");
       }
     }
