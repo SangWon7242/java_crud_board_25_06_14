cf227390bd7d00c7826fdfbc9569982263f19aac
SangWon7242 <gotkdqja@gmail.com>
2025-06-15 11:13:16 +0900

    파싱로직을 복붙해서 사용하는 것은 비효율적이다.

--- src/test/java/com/sbs/java/board/Test.java
+++ src/test/java/com/sbs/java/board/Test.java
@@ -4,29 +4,46 @@ import java.util.*;
 
 public class Test {
   public static void main(String[] args) {
-    String queryString = "page=1&searchKeyword=제목1&searchKeywordTypeCode=subject&boardId=1";
-    String[] queryStringBits = queryString.split("&");
+    String queryString1 = "page=1&searchKeyword=제목1&searchKeywordTypeCode=subject&boardId=1";
 
-    // Map<String, String> params = new HashMap<>();
-    Map<String, String> params = new LinkedHashMap<>();
+    // 압축 해제 시작
+    String[] queryStringBits1 = queryString1.split("&");
 
-    for (String bit : queryStringBits) {
+    Map<String, String> params1 = new LinkedHashMap<>();
+
+    for (String bit : queryStringBits1) {
       String[] bitBits = bit.split("=");
       String paramName = bitBits[0];
       String paramValue = bitBits[1];
 
-      params.put(paramName, paramValue);
+      params1.put(paramName, paramValue);
     }
+    // 압축 해제 끝
 
-    System.out.println(params);
-
-    System.out.println("== 원하는 것을 하나씩 뽑아오기 ==");
-    System.out.printf("page : %d\n", Integer.parseInt(params.get("page")));
-    System.out.printf("searchKeyword : %s\n", params.get("searchKeyword"));
-    System.out.printf("searchKeywordTypeCode : %s\n", params.get("searchKeywordTypeCode"));
-    System.out.printf("boardId : %d\n", Integer.parseInt(params.get("boardId")));
+    System.out.println(params1);
 
     System.out.println("== 반복문을 이용한 데이터 순회 ==");
-    params.forEach((key, value) -> System.out.printf("%s : %s\n", key, value));
+    params1.forEach((key, value) -> System.out.printf("%s : %s\n", key, value));
+
+    String queryString2 = "page=2&searchKeyword=내용1&searchKeywordTypeCode=content&boardId=2";
+
+    // 압축 해제 시작
+    String[] queryStringBits2 = queryString2.split("&");
+
+    Map<String, String> params2 = new LinkedHashMap<>();
+
+    for (String bit : queryStringBits2) {
+      String[] bitBits = bit.split("=");
+      String paramName = bitBits[0];
+      String paramValue = bitBits[1];
+
+      params2.put(paramName, paramValue);
+    }
+    // 압축 해제 끝
+
+    System.out.println(params2);
+
+    System.out.println("== 반복문을 이용한 데이터 순회 ==");
+    params2.forEach((key, value) -> System.out.printf("%s : %s\n", key, value));
   }
 }
